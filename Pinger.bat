cf20b840c7990107951fb3d61bebe1c0cc76fd72
Yonder711 <102565507+Yonder711@users.noreply.github.com>
2023-02-12 16:55:30 -0500

    Update Pinger.bat

--- Pinger.bat
+++ Pinger.bat
@@ -5,4 +5,4 @@ Set "LogFile=PingResults.txt"
 
 If Not Exist "%ServerList%" Exit /B
 >"%LogFile%" (For /F UseBackQ %%A In ("%ServerList%"
-) Do Ping -n 1 %%A|Find "TTL=">Nul&&(Echo Yes [%%A])||Echo No [%%A])
+) Do Ping -n 1 %%A|Find "TTL=">Nul&&(Echo [%%A] Yes)||Echo [%%A] No)
